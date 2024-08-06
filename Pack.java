879abc205a264c887eccc69e1c0aaa00607026b8
Subham Maity <113278914+Hallonik@users.noreply.github.com>
2024-08-07 02:54:42 +0530

    Add files via upload

---
+++ Pack.java
@@ -0,0 +1,13 @@
+package mypack1;
+
+
+
+    public class Pack {
+       public Pack() {
+       }
+    
+       public void display() {
+          System.out.println("Welcome to demo");
+       }
+    }
+    
---
+++ Pack2.java
@@ -0,0 +1,9 @@
+package mypack1;
+
+public class Pack2
+{
+       public  void display()
+        {
+             System.out.println("Welcome to demo2");
+        }
+}
